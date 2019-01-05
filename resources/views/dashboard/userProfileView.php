c41ed964797497e8dfb6b3a1f40f428f57e31f1b
mohamedalzahaby <mohamedalzahaby@gmail.com>
2019-01-05 14:34:44 +0200

    profile with img
    image made changes in database
    please remind me to discuss it when ur available

--- resources/views/dashboard/userProfileView.php
+++ resources/views/dashboard/userProfileView.php
@@ -5,10 +5,11 @@ include('resources/views/layouts/layout.php');
 <title>profile</title>
 <body style="background:none !important;">
 
-  <div>
-    <p> <span style="font-weight: bold;  display: inline;">Name : </span>  <?php echo $data[0]['name']; ?>  </p>
-    <p> <span style="font-weight: bold;  display: inline;">Email : </span> <?php echo $data[0]['email']; ?>  </p>
-    <p> <span style="font-weight: bold;  display: inline;">Gender :</span> <?php echo $data[0]['gender']; ?>  </p>
+  <div style="margin:10px;">
+  <img src="<?php echo $data[0]['avatar'];?>" alt="userimg" style="border: 1px solid #ddd; border-radius: 4px; padding: 5px; width: 150px;" >
+    <p style="display: inline-block;margin-right:10px;"> <span style="font-weight: bold;">Name : </span>  <?php echo $data[0]['name']; ?>  </p>
+    <p style="display: inline-block;margin-right:10px;"> <span style="font-weight: bold;">Email : </span> <?php echo $data[0]['email']; ?>  </p>
+    <p style="display: inline-block;margin-right:10px;"> <span style="font-weight: bold;">Gender :</span> <?php echo $data[0]['gender']; ?>  </p>
   </div>
   
   <table id="dtBasicExample" class="table table-striped table-bordered" cellspacing="0" width="100%">
