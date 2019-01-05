1210d19cf30ca239a9278ae9e9ac589478b83a90
mohamedalzahaby <mohamedalzahaby@gmail.com>
2019-01-05 14:09:16 +0200

    profile by foreach loop

--- app/User.php
+++ app/User.php
@@ -43,18 +43,18 @@ class User
 		$User = $this->config->getInstance();
 		$query = $User->query("SELECT * 
 								FROM `order`o 
+								INNER JOIN  `user` u 
 								INNER JOIN `product`p 
-								INNER JOIN `user` u 
 								INNER JOIN `gender` g
 								INNER JOIN `category` c 
 								INNER JOIN `status` s 
 								ON 
 								( 
-									(u.id = o.user_id ) AND 
-									(g.id = u.gender_id ) AND 
+									(u.id = o.user_id)    AND 
+									(g.id = u.gender_id)  AND 
 									(p.id = o.product_id) AND 
-									(s.id = o.status_id ) AND 
-									(c.id= p.category_id)
+									(s.statusId = o.status_id)  AND
+									(c.id = p.category_id)
 								) 
 								WHERE u.id = '$id'");
 		return $query->fetchAll(PDO::FETCH_ASSOC);
